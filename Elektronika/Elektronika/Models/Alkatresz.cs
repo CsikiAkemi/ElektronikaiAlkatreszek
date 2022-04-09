c9bc785cbd1f8f274c66f0df781636952b15d302
CsikiAkemi <sisina.akemi@gmail.com>
2022-04-09 17:35:56 +0200

    Nincs benne login :(

---
+++ Elektronika/Elektronika/Models/Alkatresz.cs
@@ -0,0 +1,12 @@
+﻿namespace Elektronika.Models
+{
+    public class Alkatresz
+    {
+        public int Id { get; set; }
+        public string? Megnevezes { get; set; }
+        public string? Gyarto { get; set; }
+        public string? Tipus { get; set; }
+
+        public decimal Beszar { get; set; }
+    }
+}
---
+++ Elektronika/Elektronika/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Elektronika.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elektronika.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ElektronikaContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ElektronikaContext>>()))
+            {
+                if (context == null || context.Alkatresz == null)
+                {
+                    throw new ArgumentNullException("Null ElektronikaContext");
+                }
+
+
+                if (context.Alkatresz.Any())
+                {
+                    return;
+                }
+
+                context.Alkatresz.AddRange(
+                    new Alkatresz
+                    {
+                        Megnevezes = "Processor",
+                        Gyarto = "Asus",
+                        Tipus = "AMD Ryzen 5",
+                        Beszar = 72000
+                    },
+
+                    new Alkatresz
+                    {
+                        Megnevezes = "nvme",
+                        Gyarto = "Samsung",
+                        Tipus = "PRO NVMe 2TB M. 2 PCIe",
+                        Beszar = 42920
+                    },
+
+                    new Alkatresz
+                    {
+                        Megnevezes = "Ssd",
+                        Gyarto = "Kingston",
+                        Tipus = "A400 2.5",
+                        Beszar = 9900
+                    },
+
+                    new Alkatresz
+                    {
+                        Megnevezes = "Ram",
+                        Gyarto = "Kingstone",
+                        Tipus = "2GB DDR3 1333MHz",
+                        Beszar = 3600
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
